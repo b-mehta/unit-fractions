d89373b333719cb58e97598238f641d5304d75c5
Eric Rodriguez <ericrboidi@gmail.com>
2022-05-25 23:58:05 +0100

    some more

--- src/standalone.lean
+++ src/standalone.lean
@@ -25,6 +25,7 @@ lemma sum_bUnion_le {f : ℕ → ℚ} {s : finset ℕ} {t : ℕ → finset ℕ}
 (hf : ∀ (i : ℕ), 0 ≤ f i) :
 (s.bUnion t).sum (λ (x : ℕ), f x) ≤ s.sum (λ (x : ℕ), (t x).sum (λ (i : ℕ), f i)) :=
 begin
+  -- card_bUnion_le is this but worse
   induction s using finset.induction_on with k s hks ih,
   { simp },
   rw [sum_insert hks, bUnion_insert],
@@ -61,15 +62,77 @@ end
 lemma two_in_Icc {a b x y: ℤ} (hx : x ∈ Icc a b) (hy : y ∈ Icc a b) : (|x-y|:ℝ) ≤ b-a :=
 two_in_Icc' _ rfl hx hy
 
-lemma omega_div {a b : ℕ} (h: b ∣ a) : (ω a:ℝ)- ω b ≤ ω (a/b) := sorry
+lemma omega_div {a b : ℕ} (h: b ∣ a) : (ω a:ℝ) - ω b ≤ ω (a/b) :=
+begin
+  rcases a.eq_zero_or_pos with rfl | ha,
+  { simp },
+  rcases b.eq_zero_or_pos with rfl | hb,
+  { simp [zero_dvd_iff.mp h] },
+  rw sub_le_iff_le_add,
+  norm_cast,
+  simp only [nat.arithmetic_function.card_distinct_factors_apply],
+  rw [add_comm, ←list.length_append],
+  apply list.subperm.length_le,
+  obtain ⟨k, rfl⟩ := h,
+  have hk : 0 < k := pos_of_mul_pos_left ha hb.le,
+  rw [k.mul_div_cancel_left hb],
+  refine (list.nodup_dedup _).subperm _,
+  intros x hx,
+  rw [list.mem_dedup, nat.mem_factors_mul hb.ne' hk.ne'] at hx,
+  rwa [list.mem_append, list.mem_dedup, list.mem_dedup]
+end
 
+-- this isn't true: ω 13 = 1, ω (13 / 2) = 2
 lemma omega_div_le {a b : ℕ} : ω (a/b) ≤ ω a := sorry
 
-lemma omega_mul_ppower {a q : ℕ} (hq : is_prime_pow q) : ω (q*a) ≤ 1 + ω a := sorry
+section list
+
+open list
+
+lemma list.mem_join_repeat {α} {n : ℕ} {a : list α} {t} (hn : n ≠ 0) :
+  t ∈ (list.repeat a n).join ↔ t ∈ a :=
+begin
+  simp_rw [list.mem_join, list.mem_repeat],
+  exact ⟨by { rintro ⟨a, ⟨hn, rfl⟩, h⟩, exact h }, λ h, ⟨_, ⟨hn, rfl⟩, h⟩⟩
+end
+
+lemma nat.factors_pow_perm (a : ℕ) {n : ℕ} (hn : n ≠ 0) :
+  (a ^ n).factors ~ (list.repeat a.factors n).join :=
+begin
+  rcases eq_or_ne a 0 with rfl | ha,
+  { simpa using or.inl hn.bot_lt },
+  refine (nat.factors_unique _ $ λ p hp, _).symm,
+  { simp [nat.prod_factors ha] },
+  rw [list.mem_join_repeat hn, nat.mem_factors ha] at hp,
+  exact hp.1
+end
+
+lemma nat.factors_pow (a : ℕ) {n : ℕ} (hn : n ≠ 0) :
+  (a ^ n).factors = (list.repeat a.factors n).join.merge_sort (≤) :=
+eq_of_perm_of_sorted ((a.factors_pow_perm hn).trans (perm_merge_sort _ _).symm)
+                     ((a ^ n).factors_sorted) $ sorted_merge_sort _ _
+
+end list
+
+lemma omega_pow {a n : ℕ} (hn : n ≠ 0) : ω a = ω (a ^ n) :=
+begin
+  simp only [nat.arithmetic_function.card_distinct_factors_apply],
+  apply list.perm.length_eq,
+  rw [list.perm_ext (list.nodup_dedup _) (list.nodup_dedup _)],
+  intro x,
+  rw [list.mem_dedup, list.mem_dedup, (a.factors_pow_perm hn).mem_iff, list.mem_join_repeat hn]
+end
+
+lemma omega_mul_ppower {a q : ℕ} (hq : is_prime_pow q) : ω (q*a) ≤ 1 + ω a :=
+begin
+  obtain ⟨p, n, hp, hn, rfl⟩ := hq,
+  sorry,
+end
 
 lemma sum_add_sum {A B : finset ℕ} {f : ℕ → ℝ} :
 A.sum (λ (i : ℕ), f i) + B.sum (λ (i : ℕ), f i) = (A∪B).sum (λ (i : ℕ), f i) +
-(A∩B).sum (λ (i : ℕ), f i) := sorry
+(A∩B).sum (λ (i : ℕ), f i) :=
+by rw sum_union_inter
 
 lemma sum_add_sum_add_sum {A B C : finset ℕ} {f : ℕ → ℝ} :
 A.sum (λ (i : ℕ), f i) + B.sum (λ (i : ℕ), f i) + C.sum (λ (i : ℕ), f i) =
@@ -79,7 +142,8 @@ A.sum (λ (i : ℕ), f i) + B.sum (λ (i : ℕ), f i) + C.sum (λ (i : ℕ), f i
 
 lemma sum_le_sum_of_inj {A B : finset ℕ} {f1 f2 : ℕ → ℝ} (g : ℕ → ℕ) (hf2 : ∀ b ∈ B, 0 ≤ f2 b )
 (hgB : ∀ a ∈ A, g a ∈ B) (hginj : ∀ a1 a2 ∈ A, (g a1 = g a2) → a1 = a2) (hgf : ∀ a ∈ A, f2 (g a) = f1 a) :
-A.sum (λ (i : ℕ), f1 i) ≤ B.sum (λ (i : ℕ), f2 i) := sorry
+A.sum (λ (i : ℕ), f1 i) ≤ B.sum (λ (i : ℕ), f2 i) :=
+sorry
 
 lemma eq_iff_ppowers_dvd (a b  : ℕ) : a = b ↔ (∀ q ∣ a, is_prime_pow q → coprime q (a/q)
  → q ∣ b) ∧ (∀ q ∣ b, is_prime_pow q → coprime q (b/q) → q ∣ a) := sorry
